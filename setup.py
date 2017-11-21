bb7a84da1f2f2e5af6c9bd461f63396151fa53ab
sergio-verduzco <sergio.verduzco@oist.jp>
2017-11-21 17:39:31 +0900

    Fix Cython setup file. Write code for firing rate histogram animations in rate_density.ipynb . Almost finished code for dual histogram and activity animations.

--- setup.py
+++ setup.py
@@ -3,10 +3,20 @@ setup.py
 
 This is the distutils setup file used to build the Cython utilities used by draculab.
 """
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy
+
+setup(
+    ext_modules=[
+        Extension("cython_utils", ["cython_utils.c"],
+            include_dirs=[numpy.get_include()]),
+    ],
+)
+
 
 setup(
     name = 'draculab cythonized functions',
     ext_modules = cythonize("cython_utils.pyx"),
+    include_dirs=[numpy.get_include()]
 )
